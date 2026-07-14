0e0d1d76a6f23c3f10ea2a1ddea80770ffbc88fa
itpcmc2024 <itpcmc2024@gmail.com>
2026-07-14 12:39:53 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   // ใส่ URL Web App ของ Apps Script V5 หลัง Deploy
-  API_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE",
+  API_URL: "https://script.google.com/macros/s/AKfycbzZ1uAGlGItaXHV2GSPLaiC3JqDAmWC6vHnh8hqfdGx8GhlDhDf00pfKFjouDL3gdEILA/exec",
   APP_NAME: "New Items V5",
   DEFAULT_SHEET: ""
 };
