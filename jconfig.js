a26e5f6a8937f7192a2a3723905a279c7cb51176
itpcmc2024 <itpcmc2024@gmail.com>
2026-07-14 11:36:04 +0700

    Create jconfig.js

---
+++ jconfig.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG = {
+  // ใส่ URL Web App ของ Apps Script V5 หลัง Deploy
+  API_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE",
+  APP_NAME: "New Items V5",
+  DEFAULT_SHEET: ""
+};
